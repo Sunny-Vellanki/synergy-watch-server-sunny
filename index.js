5af86d3544a9c0cb58bd39d8176af923e5bed626
Sunny-Vellanki <sunnyvellanki1415@gmail.com>
2024-04-19 12:51:34 +0530

    deploy server

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Userdetails = require("./models/signupdetails");
 app.use(express.json());
 app.use(cors());
 
-//mangodb connect
+//mangodb connect to database
 mongoose.connect(`mongodb+srv://sunny:sunnyh123@cluster0.x0yawgr.mongodb.net/SynergyProject?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
     console.log("database connected")
